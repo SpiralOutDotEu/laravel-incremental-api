02cdaeb322b5192dc593a1d2c36d686d79e6196b
Spiral Out <spiralout.eu@gmail.com>
2016-03-25 20:39:18 +0200

    artisan make:auth so that i can register new user quickly. Not in Laracast

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,3 +32,9 @@ Route::group(['middleware' => ['web']], function () {
     });
 
 });
+
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
+
+    Route::get('/home', 'HomeController@index');
+});
